69ddf3881221961beedbbc3cf3c4ff3aab1e7a94
Lorenzo Maturano <maturanolorenzo@gmail.com>
2019-09-06 13:15:59 -0300

    adding players and roulette class

--- roleta.py
+++ roleta.py
@@ -11,20 +11,25 @@ class Game:
 		self.selectedRoulette = None
 		self.numberOfPlayers = 0
 
-		self.selectWheelType()
+		self.selectRouletteType()
 		self.selectNumberOfPlayers()
 		self.showGameSettings()
+		self.instantiatePlayers()
 
-	def selectWheelType(self):
 
+	def selectRouletteType(self):
+		#Method for selecting roulette type
 		while self.selectedRoulette not in ['1','2','3']:
 			self.selectedRoulette = input('Select game style:\n\n1-American Roulette\n2-European Roueltte\n3-French Roulette\n')
 			if self.selectedRoulette == '1':
 				print(f'Ok! Loading the {self.rouletteDict[self.selectedRoulette]}\n')
+				roulette = AmericanRoulette()
 			elif self.selectedRoulette == '2':
 				print(f'Ok! Loading the {self.rouletteDict[self.selectedRoulette]}\n')
+				roulette = EuropeanRoulette()
 			elif self.selectedRoulette == '3':
 				print(f'Ok! Loading the {self.rouletteDict[self.selectedRoulette]}\n')
+				roulette = FrenchRoulette()
 			else:
 				print('You must select the Roulette by typing 1, 2 or 3\n')
 
@@ -36,6 +41,14 @@ class Game:
 	def showGameSettings(self):
 		print(f'Setting {self.rouletteDict[self.selectedRoulette]} game for {self.numberOfPlayers} player(s)!\n')
 
+	def instantiatePlayers(self):
+		global players
+		for player in range(1,self.numberOfPlayers + 1):
+			name = input(f"Player {player}, enter your name: ")
+			players.addPlayer(name)
+
+
+
 class Roulette:
 	def __init__(self):
 		self.board = [str(i) for i in range(0,37)]
@@ -43,11 +56,16 @@ class Roulette:
 
 class Players:
 	def __init__(self):
-		pass
+		self.playersList = []
+
+	def addPlayer(self,name):
+		self.playersList.append(Player(name))
 
 class Player:
-	def __init__(self):
-		pass
+	def __init__(self,name):
+		self.name = name
+		self.bank = 100
+
 
 class AmericanRoulette(Roulette):
 	def __init__(self):
@@ -62,5 +80,9 @@ class FrenchRoulette(Roulette):
 	def __init__(self):
 		super().__init__()
 
-
+players = Players()
 game = Game()
+for player in players.playersList:
+	print(player.name)
+	print(player.bank)
+	print('')
