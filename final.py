e5de331d7944add59194bf9a4aaf1f46a5e03bdc
Lorenzo Maturano <maturanolorenzo@gmail.com>
2019-09-26 15:39:31 -0300

    made new class to represent inner bets

--- final.py
+++ final.py
@@ -109,6 +109,7 @@ class Player:
         self.betTypeChoice = None
         self.outsideBetId = None
         self.insideBetCategory = None
+        self.insideBetChoice = None
 
     def  __repr__(self):
         return f'{self.name}'
@@ -159,19 +160,27 @@ class Player:
                     print('\nYou need to select your bet category by typing a interger (1-7)')
             #Straight/Single
             if self.insideBetCategory == '1':
+                #Display options
+                # x = [number['id'] for number in game.roulette.board]
+                # print(x)
                 pass
+                #Make choice
             #Split
             elif self.insideBetCategory == '2':
-                pass
+                #Display Options
+                insideBetChoices.displayOptions(insideBetChoices.splits)
+                choice = input('Choose your bet: ')
+                #Make Choice
             #Street
             elif self.insideBetCategory == '3':
-                pass
+                insideBetChoices.displayOptions(insideBetChoices.streets)
             #Corner/Square
             elif self.insideBetCategory == '4':
-                pass
+                insideBetChoices.displayOptions(insideBetChoices.squares)
             #Six Line/Double Street
             elif self.insideBetCategory == '5':
-                pass
+                insideBetChoices.displayOptions(insideBetChoices.doubleStreets)
+                choice = input('Choose your bet: ')
             #Trio (american)
             elif self.insideBetCategory == '6':
                 pass
@@ -188,16 +197,17 @@ class Player:
                 pass
             #Split
             elif self.insideBetCategory == '2':
-                pass
+                insideBetChoices.displayOptions(insideBetChoices.splits)
+
             #Street
             elif self.insideBetCategory == '3':
-                pass
+                insideBetChoices.displayOptions(insideBetChoices.streets)
             #Corner/Square
             elif self.insideBetCategory == '4':
-                pass
+                insideBetChoices.displayOptions(insideBetChoices.squares)
             #Six Line/Double Street
             elif self.insideBetCategory == '5':
-                pass
+                insideBetChoices.displayOptions(insideBetChoices.doubleStreets)
             #Trio (american)
             elif self.insideBetCategory == '6':
                 pass
@@ -214,16 +224,16 @@ class Player:
                 pass
             #Split
             elif self.insideBetCategory == '2':
-                pass
+                insideBetChoices.displayOptions(insideBetChoices.splits)
             #Street
             elif self.insideBetCategory == '3':
-                pass
+                insideBetChoices.displayOptions(insideBetChoices.streets)
             #Corner/Square
             elif self.insideBetCategory == '4':
-                pass
+                insideBetChoices.displayOptions(insideBetChoices.squares)
             #Six Line/Double Street
             elif self.insideBetCategory == '5':
-                pass
+                insideBetChoices.displayOptions(insideBetChoices.doubleStreets)
             #Trio (american)
             elif self.insideBetCategory == '6':
                 pass
@@ -357,6 +367,23 @@ class FrenchRoulette(Roulette):
 
     def __repr__(self):
         return 'French Roulette'
+
+class InsideBetChoices:
+    def __init__(self):
+        self.splits = [[1,2],[2,3],[4,5],[5,6],[7,8],[8,9],[10,11],[11,12],[13,14],[14,15],[16,17],[17,18],[19,20],[20,21],[22,23],[23,24],[25,26],[26,27],[28,29],[29,30],[31,32],[32,33],[34,35],[35,36],[1,4],[4,7],[7,10],[10,13],[13,16],[19,22],[22,25],[25,28],[28,31],[31,34],[2,5],[5,8],[8,11],[11,14],[14,17],[17,20],[20,23],[23,26],[26,29],[29,32],[32,35],[3,6],[6,9],[9,12],[12,15],[15,18],[18,21],[21,24],[24,27],[27,30],[30,33],[33,36]]
+        self.streets = [[1,2,3],[4,5,6],[7,8,9],[10,11,12],[13,14,15],[16,17,18],[19,20,21],[22,23,24],[25,26,27],[28,29,30],[31,32,33],[34,35,36]]
+        self.squares = [[1,2,4,5],[2,3,5,6],[4,7,5,8],[5,6,7,9],[7,8,10,11],[8,9,11,12],[10,11,13,14],[11,12,14,15],[13,14,16,17],[14,15,17,18],[16,17,19,20],[17,18,20,21],[19,20,22,23],[20,21,23,24],[22,23,25,26],[23,24,26,27],[25,26,28,29],[26,27,29,30],[28,29,31,31],[29,30,32,33],[31,32,34,35],[32,33,35,36]]
+        self.doubleStreets = [[1,2,3,4,5,6],[4,5,6,7,8,9],[7,8,9,10,11,12],[10,11,12,13,14,15],[13,14,15,16,17,18],[16,17,18,19,20,21],[19,20,21,22,23,24],[22,23,24,25,26,27],[25,26,27,28,29,30],[28,29,30,31,32,33],[31,32,33,34,35,36]]
+
+    def displayOptions(self,list):
+        i = 0
+        print('\nThis are your options\n')
+        for group in list:
+            print(f'{i}: {group}')
+            i+=1
+
+
+insideBetChoices = InsideBetChoices()
 
 game = Game()
 game.runGame()
--- tes.py
+++ tes.py
@@ -1,48 +1,134 @@
-#Cada numero esta relacionado
-    #3 propriedades binarias
-        #Paridade
-        #Cor
-        # 1 <= n <= 18 ou nao
-    #2 propriedades ternarias
-        #Primeiros 12, Segundos 12, Terceiros 12
-        #Primeira coluna, Segunda coluna, Terceira coluna
-    #A excecao de tudo isso eh o zero (ou o 00, no caso da roleta americana)
-        #Ele nao se associa a nenhuma categoria (paridade, cor,...)
-
-#Como modelar essas informacoes
-    #O que parece ser mais adequado e utilizar dicionarios
-    #cada numero esta associado a um dicionario, esse dicionario contem as informacoes de cada numberOfPlayers
-        #Se eh par ou impar, se eh preto ou branco, qual metade pertence,...
-
-# exemplo = {
-#     'id': 1,
-#     'even': False,
-#     'red': True,
-#     'lessOrEqual18': True,
-#     'firstTwelve': True,
-#     'secondTwelve': False,
-#     'thirdTwelve': False,
-#     'firstColumn': True,
-#     'secondColumn': False,
-#     'thirdColumn': False,
-# }
-# if 0 in range(1,37,3):
-#     print('Yes!')
-# else:
-#     print('No!')
-
-class Mae:
+class Roulette:
     def __init__(self):
-        self.teste = 'oi'
-        # self.digaOi()
-        self.digaOi()
-    def oi(self):
-        print('Oi')
-        
-    def digaOi(self):
-        self.oi()
-class Filho(Mae):
+        self.sortedNumber = None
+        self.bank = 1000
+        self.board = self.setBoard()
+
+
+    def setBoard(self):
+        self.board = []
+        for number in range(1,37):
+            self.board.append(
+                {
+                    'id': str(number),
+                    'even': self.determineIfEven(number),
+                    'red': self.determineIfRed(number),
+                    'lessOrEqual18': self.determineIfLessOrEqualThan18(number),
+                    'firstTwelve':  self.determineIfFirst12(number),
+                    'secondTwelve': self.determineIfSecond12(number),
+                    'thirdTwelve': self.determineIfThird12(number),
+                    'firstColumn': self.determineIfFirstColumn(number),
+                    'secondColumn': self.determineIfSecondColumn(number),
+                    'thirdColumn': self.determineIfThirdColumn(number)
+                }
+            )
+        self.board.insert(0,
+            {
+                'id': '0',
+                'even': False,
+                'red': False,
+                'lessOrEqual18': False,
+                'firstTwelve':  False,
+                'secondTwelve': False,
+                'thirdTwelve': False,
+                'firstColumn': False,
+                'secondColumn': False,
+                'thirdColumn': False
+            }
+        )
+        return self.board
+
+    def determineIfEven(self,number):
+        if number % 2 == 0:
+            return True
+        return False
+
+    def determineIfRed(self,number):
+        if number in [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]:
+            return True
+        return False
+
+    def determineIfLessOrEqualThan18(self,number):
+        if number <= 18:
+            return True
+        return False
+
+    def determineIfFirst12(self,number):
+        if 1<=number<=12:
+            return True
+        return False
+
+    def determineIfSecond12(self,number):
+        if 13<=number<=24:
+            return True
+        return False
+
+    def determineIfThird12(self,number):
+        if 25<=number:
+            return True
+        return False
+
+    def determineIfFirstColumn(self,number):
+        if number in range(1,37,3):
+            return True
+        return False
+
+    def determineIfSecondColumn(self,number):
+        if number in range(2,37,3):
+            return True
+        return False
+
+    def determineIfThirdColumn(self,number):
+        if number in range(3,37,3):
+            return True
+        return False
+
+class AmericanRoulette(Roulette):
+    def __init__(self):
+        super().__init__()
+        self.board.insert(0,
+            {
+                'id': '00',
+                'even': False,
+                'red': False,
+                'lessOrEqual18': False,
+                'firstTwelve':  False,
+                'secondTwelve': False,
+                'thirdTwelve': False,
+                'firstColumn': False,
+                'secondColumn': False,
+                'thirdColumn': False
+            }
+        )
+
+    def __repr__(self):
+        return 'American Roulette'
+
+class EuropeanRoulette(Roulette):
     def __init__(self):
         super().__init__()
 
-filho = Filho()
+    def __repr__(self):
+        return 'European Roulette'
+
+class FrenchRoulette(Roulette):
+    def __init__(self):
+        super().__init__()
+
+    def __repr__(self):
+        return 'French Roulette'
+
+
+roleta = Roulette()
+roletaAmericana = AmericanRoulette()
+roletaEuropeia = EuropeanRoulette()
+roletaFrancesa = FrenchRoulette()
+
+x = [number['id'] for number in roletaAmericana.board]
+print(x)
+
+# for number in roletaAmericana.board:
+#     print(number['id'])
+# bet = None
+# while bet not in (number['id'] for number in roletaAmericana.board):
+#     bet = input('Faça sua aposta istepo')
